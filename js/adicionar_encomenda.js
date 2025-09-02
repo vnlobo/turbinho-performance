cbc9debf84f88ce79e99f14f686801b6b6ab2f1a
Vinicius Lôbo <viniciusd.lobo@gmail.com>
2025-09-01 22:06:46 -0300

    funcionalidade para adicionar novas encomendas

--- js/adicionar_encomenda.js
+++ js/adicionar_encomenda.js
@@ -1,10 +1,55 @@
 var botaoAdicionar = document.querySelector("#adicionar");
 
-var linha = document.createElement("tr");
 
-var coluna = document.createElement("td");
-
-botaoAdicionar.addEventListener("click", function(event){
+botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
-    console.log("vazio")
+
+    // capturar formulário
+    var form = document.querySelector("#adicionar-encomenda");
+
+    // captura a nova encomenda
+    var novaEncomenda = obterEncomenda(form);
+
+    // captura a tabela encomenda
+    var tabela = document.querySelector("#tabela-cliente");
+
+    // insere a nova encomenda na tabela
+    tabela.appendChild(montaLinha(novaEncomenda));
+
+    // limpa o formulário
+    form.reset();
 });
+
+// captura os dados do formulário
+function obterEncomenda(formulario) {
+    var encomenda = {
+        cliente: formulario.nome.value,
+        qtde: formulario.qtde.value,
+        produto: formulario.produto.value,
+        unitario: formulario.unitario.value
+    }
+
+    return encomenda;
+}
+
+// função criar nova linha (tr)
+function montaLinha(dadosNovaEncomenda) {
+    var linha = document.createElement('tr');
+
+    linha.appendChild(montaColuna(dadosNovaEncomenda.cliente));
+    linha.appendChild(montaColuna(dadosNovaEncomenda.produto));
+    linha.appendChild(montaColuna(dadosNovaEncomenda.qtde));
+    linha.appendChild(montaColuna(formataValor(dadosNovaEncomenda.unitario)));
+    linha.appendChild(montaColuna(calculaTotal(dadosNovaEncomenda.qtde, dadosNovaEncomenda.unitario)));
+
+    return linha;
+}
+
+// função criar nova coluna (td)
+function montaColuna(dadosColuna) {
+    var coluna = document.createElement('td');
+
+    coluna.textContent = dadosColuna;
+
+    return coluna;
+}
